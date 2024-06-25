3c0f06b3a9994334d0402d2030e8a17c32084259
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-25 22:45:50 +0530

    updated the header where I attached the link of the navbar parts, used usePathname for the route path text color appears only on the tab which is active.

--- real-estate-website/src/components/Header.tsx
+++ real-estate-website/src/components/Header.tsx
@@ -1,15 +1,27 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button } from './ui/button'
 import {Plus} from "lucide-react"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 function Header() {
+
+  const path = usePathname();
+
+  useEffect(()=> {
+    console.log(path);
+    
+  },[])
+
   return (
     <div className='p-6 px-10 top-0 w-full z-10 bg-white flex justify-between shadow-sm fixed '>
       <div className='flex gap-12 items-center'>
         <Image src={'/next.svg'} alt='logo' width={150} height={150}/>
           <ul className='hidden md:flex gap-10'>
-            <li className='hover:text-primary font-medium text-sm cursor-pointer'>For Sale</li>
+            <Link href={'/'}><li className={`'hover:text-primary font-medium text-sm cursor-pointer' ${path=='/'&&'text-primary'}`}>For Sale</li></Link>
             <li className='hover:text-primary font-medium text-sm cursor-pointer'>For Rent</li>
             <li className='hover:text-primary font-medium text-sm cursor-pointer'>Agent Finder</li>
           </ul>
