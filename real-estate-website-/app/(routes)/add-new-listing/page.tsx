99a54b10862b5a339727a993924690061cc04d5f
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-27 09:02:11 +0530

    ohh finally got the google place map api after a long period of struggle with the google cloud console and successfully saved the api key and used it in the GooglePlacesAutocomplete apiKey section .

--- real-estate-website-/app/(routes)/add-new-listing/page.tsx
+++ real-estate-website-/app/(routes)/add-new-listing/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function AddNewListing() {
   return (
-    <div>
+    <div className='p-10 flex flex-col gap-4 justify-center items-center'>
         <h2 className='font-bold text-2xl'>Add New Listing</h2>
+        <div>
+            <h2 className='text-gray-500'>Enter Address which you want to list</h2>
+            <GooglePlacesAutocomplete
+                apiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY!}
+            />
+        </div>
     </div>
   )
 }
