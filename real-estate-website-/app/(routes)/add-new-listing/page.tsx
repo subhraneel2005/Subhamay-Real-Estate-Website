711f8ce782ea5a4c8546a350e03abc32c242a6e3
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-09 15:59:15 +0530

    updated the Google Auto Complete page and also the Google Address Search component.

--- real-estate-website-/app/(routes)/add-new-listing/page.tsx
+++ real-estate-website-/app/(routes)/add-new-listing/page.tsx
@@ -1,14 +1,20 @@
 import GoogleAddressSearch from '@/app/_components/GoogleAddressSearch';
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 function AddNewListing() {
   return (
-    <div className='p-10 flex flex-col gap-4 justify-center items-center'>
+    <div className='mt-10 md:mx-56 lg:mx-80'>
+    <div className='p-10 flex flex-col gap-5 justify-center items-center'>
         <h2 className='font-bold text-2xl'>Add New Listing</h2>
-        <div>
-            <h2 className='text-gray-500'>Enter Address which you want to list</h2>
-            <GoogleAddressSearch/>
+        
+        <div className='p-10 px-28 rounded-lg border shadow-md flex flex-col gap-5 w-full'>
+          <h2 className='text-gray-500'>Enter Address which you want to list</h2>
+          <GoogleAddressSearch/>
+          <Button>Next</Button>
         </div>
+        
+    </div>
     </div>
   )
 }
--- real-estate-website-/app/_components/GoogleAddressSearch.tsx
+++ real-estate-website-/app/_components/GoogleAddressSearch.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { MapPin } from 'lucide-react';
 import React from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 function GoogleAddressSearch() {
   return (
-    <div>
+    <div className='w-full flex items-center'>
+      <MapPin className='cursor-pointer h-10 w-10 p-2 text-primary bg-purple-200 rounded-l-lg'/>
         <GooglePlacesAutocomplete
             apiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY!}
             selectProps={{
